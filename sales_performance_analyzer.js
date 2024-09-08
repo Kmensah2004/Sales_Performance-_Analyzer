162efbf43bf171081b7d72404d0d611beff3444f
Kosi Mensah <kmensah@usf.edu>
2024-09-08 12:28:32 -0400

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,4 +1,5 @@
 function calculateAverageSales(sales) {
+    if (sales.length === 0) return 0;
     const totalSales = sales.reduce((total, sale) => total + sale, 0);
     return totalSales / sales.length;
 }
@@ -13,3 +14,18 @@ function determinePerformanceRating(averagesales){
         return "Needs Improvement";
     }
 }
+function findTopAndBottomPerformers(salesData) {
+    if (salesData.length === 0) return { topPerformer: null, bottomPerformer: null };
+    }
+    let topPerformer = salesData[0];
+    let bottomPerformer = salesData[0];
+
+    salesData.reduce((salesperson) => {
+        if (salesperson.totalSales > topPerformer.totalSales) {
+            topPerformer = salesperson;
+        }
+        else if (salesperson.totalSales < bottomPerformer.totalSales) {
+            bottomPerformer = salesperson;
+        }
+    },);
+    return { topPerformer, bottomPerformer };
