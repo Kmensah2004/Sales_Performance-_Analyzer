c3135591ed1afbad4c88f5f1c9b5e7a634a0a430
Kosi Mensah <kmensah@usf.edu>
2024-09-07 22:27:04 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -2,3 +2,14 @@ function calculateAverageSales(sales) {
     const totalSales = sales.reduce((total, sale) => total + sale, 0);
     return totalSales / sales.length;
 }
+function determinePerformanceRating(averagesales){
+    if (averagesales > 1000){
+        return "Excellent";
+    } else if (averagesales >= 7000 && averagesales <= 10000) {
+        return "Good";
+    } else if (averagesales >= 4000 && averagesales < 7000) {
+        return "Satisfactory";
+    } else {
+        return "Needs Improvement";
+    }
+}
